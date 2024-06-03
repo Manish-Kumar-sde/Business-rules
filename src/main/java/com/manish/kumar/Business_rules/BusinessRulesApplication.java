97e841c1ff5de497df81d5f98cb377249952c8c3
manish kumar <manish.kumar@arthmate.com>
2024-06-03 17:50:37 +0530

    Business rule engine project

---
+++ src/main/java/com/manish/kumar/Business_rules/BusinessRulesApplication.java
@@ -0,0 +1,13 @@
+package com.manish.kumar.Business_rules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessRulesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessRulesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/manish/kumar/Business_rules/BusinessRulesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manish.kumar.Business_rules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessRulesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
