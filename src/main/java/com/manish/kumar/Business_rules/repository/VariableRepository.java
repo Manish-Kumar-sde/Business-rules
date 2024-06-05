844c379ef4181e096d4028860cb1b6de386b8ae6
manish kumar <manish.kumar@arthmate.com>
2024-06-05 18:59:06 +0530

    variable saved in db

---
+++ src/main/java/com/manish/kumar/Business_rules/repository/VariableRepository.java
@@ -0,0 +1,7 @@
+package com.manish.kumar.Business_rules.repository;
+
+import com.manish.kumar.Business_rules.model.Variable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VariableRepository extends JpaRepository<Variable,Integer> {
+}
